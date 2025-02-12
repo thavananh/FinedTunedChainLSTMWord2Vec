13f665e521e3f530b01cfd1b4c01fb5c59c8be6b
duycute <thavananh@gmail.com>
2025-02-12 13:33:20 +0700

    12323

--- main.py
+++ main.py
@@ -128,6 +128,10 @@ def main():
     parser.add_argument(
         "--use_simple", action="store_true", help="Simple spliting text"
     )
+    parser.add_argument(
+        ""
+    )
+
 
     args = parser.parse_args()
 
